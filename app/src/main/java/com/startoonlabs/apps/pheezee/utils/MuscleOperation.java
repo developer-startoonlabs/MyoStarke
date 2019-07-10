d2ecb608524049f31958a02472160643bf412b54
anku_802 <ankushsharma8210@gmail.com>
2019-07-10 12:03:00 +0530

    bugfix and updated the muscle names as per the documents 2.1.5

--- app/src/main/java/com/startoonlabs/apps/pheezee/utils/MuscleOperation.java
+++ app/src/main/java/com/startoonlabs/apps/pheezee/utils/MuscleOperation.java
@@ -2,27 +2,29 @@ package com.startoonlabs.apps.pheezee.utils;
 
 public class MuscleOperation {
     public static String[][] musle_names = {
-            {"Select Muscle", "Long Head of Biceps", "Short head of biceps", "Brachialis", "Lateral Head of Triceps","Long Head of Triceps",
-                    "Medial Head of Triceps","Brachioradialis", "Anconeus"},//elbow
+            {"Select Muscle", "Biceps", "Brachialis", "Triceps","Brachioradialis", "Anconeus", "Infraspinatus", "Teres Minor", "Pectoralis Major",
+                    "Pectoralis Minor", "Serratus Anterior", "Trapezius"},//elbow
 
-            {"Select Muscle", "Quadratus Femoris Obturator Externus", "Biceps Femoris-Posterior", "Semimembranosus-Posterior", "Semitendinosus-Posterior",
-                    "Rectus Femoris-Anterior", "Vastus Lateralis-Anterior", "Vastus Medialis -Anterior", "Vastus Intermedius-Anterior",
-                    "Sartorius Anterior", "Pectineus-Medial" }, //Knee
+            {"Select Muscle", "Rectus Femoris-Anterior", "Vastus Lateralis-Anterior", "Vastus Medialis -Anterior", "Vastus Intermedius-Anterior(Deep)",
+                    "Sartorius-Anterior", "Gracilis-Medial", "Biceps Femoris-Posterior", "Semimembranosus -Posterior", "Semitendinosus-Posterior", "Gastrocnemius-Posterior",
+                     }, //Knee
 
-            {"Select Muscle", "Extensor Digitorum Brevis","Abductor Hallucis", "Flexor Digitorum Brevis", "Abductor Digiti Minimi", "Quadratus Plantae", "Lumbricals(4)",
-                    "Flexor Hallucis Brevis", "Adductor Hallucis", "Flexor Digiti Minimi Brevis", "Dorsal Interossei (4)", "Plantar Interossei (3)",
-                    "Gastrocnemius-Posterior", "Soleus -Posterior", "Plantaris-Posterior", "Popliteus-Posterior", "Flexor Digitorum Longus",
-                    "Flexor Hallucis Longus", "Tibialis Posterior-Posterior", "Tibialis Anterior -Anterior", "Extensor Digitorum Longus -Anterior",
-                    "Extensor Hallucis Longus-Anterior", "Peroneus Tertius-Anterior", "Peroneus Longus-Lateral", "Peroneus Brevis-Lateral"},    //Ankle
+            {"Select Muscle", "Gastrocnemius-Posterior", "superficial-Part of Triceps Surae", "Soleus -Posterior", "Plantaris-Posterior",
+                    "Flexor Digitorum Longus(Deep)", "Flexor Hallucis Longus(Deep)", "Tibialis Posterior(Deep)", "Tibialis Anterior-Anterior",
+                    "Extensor Digitorum Longus-Anterior", "Extensor Hallucis Longus-Anterior", "Peroneus Tertius-Anterior",
+                    "Peroneus Longus-Lateral", "Peroneus Brevis-Lateral"},    //Ankle
 
-            {"Select Muscle", "Gracilis -Medial", "Gluteus Maximus-Gluteal", "Gluteus Medius And Gluteus Minimus-Gluteal", "Tensor Fasciae Latae-Gluteal ",
-                    "Piriformis", "Superior", "Gemellus", "Obturator", "Internus", "Inferior Gemellus", "Adductor Longus-Medial", "Adductor Brevis -Medial", "Adductor Magnus - Medial"},  //Hip //from 4 to 8 i have taken as different muscles as specified in the excell sheet
+            {"Select Muscle", "Rectus Femoris  -Anterior", "Sartorius-Anterior", "Pectineus-Medial(Deep)", "Gracilis -Medial", "Gluteus Maximus-Gluteal",
+                    "Gluteus Medius And Gluteus Minimus-Gluteal", "Tensor Fasciae Latae-Gluteal ", "Piriformis(Deep)", "Superior(Deep)", "Gemellus(Deep)",
+                    "Obturator(Deep)", "Internus(Deep)", "Inferior Gemellus(Deep)", "Quadratus Femoris Obturator Externus(Deep)", "Biceps Femoris - Posterior",
+                    "Semimembranosus-Posterior", "Semitendinosus-Posterior"},  //Hip //from 4 to 8 i have taken as different muscles as specified in the excell sheet
 
-            {"Select Muscle", "Flexor Carpi Radialis", "Palmaris Longus", "Flexor Carpi Ulnaris", "Flexor Digitorum Superficialis", "Flexor Pollicis Longus", "Flexor Digitorum  Profundus",
-                    "Pronator Quadratus"},  //Wrist
+            {"Select Muscle", "Flexor Carpi Radialis", "Palmaris Longus", "Flexor Carpi Ulnaris", "Flexor Pollicis Longus (Deep)",
+                    "Flexor Digitorum  Profundus (Deep)", "Pronator Quadratus (Deep)", "Extensor Carpi  Radialis Longus and Brevis", "Extensor Digiti Minimi",
+                    "Extensor Carpi Ulnaris", "Supinator (Deep)", "Pronator Teres (Deep)", "Biceps"},  //Wrist
 
-            {"Select Muscle", "Pectoralis Major", "Pectoralis Minor", "Serratus Anterior", "Trapezius", "Rhomboids", "Levator Scapulae", "Latissimus Dorsi", "Deltoid",
-                    "Teres Major", "Coracobrachialis", "Subscapularis", "Supraspinatus", "Infraspinatus", "Teres Minor"},   //Shoulder
+            {"Select Muscle", "Pectoralis Major", "Pectoralis Minor", "Serratus Anterior", "Trapezius",  "Latissimus Dorsi", "Deltoid",
+                    "Teres Major",  "Teres Minor", "Long Head of Biceps", "Short head of biceps"},   //Shoulder
             {}
     };
 
