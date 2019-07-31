b61b9c2d3639e6bf04bdeed3c21cb2f0dcfbbfe0
anku_802 <ankushsharma8210@gmail.com>
2019-07-31 16:30:42 +0530

    default commit

--- app/src/main/java/com/startoonlabs/apps/pheezee/room/Dao/MqttSyncDao.java
+++ app/src/main/java/com/startoonlabs/apps/pheezee/room/Dao/MqttSyncDao.java
@@ -7,6 +7,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 import androidx.room.Update;
 
 import com.startoonlabs.apps.pheezee.room.Entity.MqttSync;
