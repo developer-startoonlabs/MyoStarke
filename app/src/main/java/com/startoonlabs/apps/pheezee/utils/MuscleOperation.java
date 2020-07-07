77b36426336b8209429d96a8e58e4a7662f12cc6
unknown <developer.startoonlabs@gmail.com>
2020-07-07 11:49:06 +0530

    Changed button layout and correct muscle names.

--- app/src/main/java/com/startoonlabs/apps/pheezee/utils/MuscleOperation.java
+++ app/src/main/java/com/startoonlabs/apps/pheezee/utils/MuscleOperation.java
@@ -6,35 +6,35 @@ import com.startoonlabs.apps.pheezee.R;
 
 public class MuscleOperation {
     private final static String[][] musle_names = {
-            {"Select Muscle*", "Biceps", "Brachialis (deep)","Brachioradialis", "Tricep", "Anconeus", "Others"},//elbow
+            {"Select Muscle*", "Biceps", "Brachialis (Deep)","Brachioradialis", "Tricep", "Anconeus", "Others"},//elbow
 
-            {"Select Muscle*", "Sartorius - Anterior", "Gracilis -Medial", "Biceps Femoris - Posterior", "Semimembranosus -Posterior",
-                    "Semitendinosus -Posterior", "Popliteus(Posterior, deep)", "Gastrocnemius-Posterior", "Rectus Femoris - Anterior",
+            {"Select Muscle*", "Sartorius - Anterior", "Gracilis - Medial", "Biceps Femoris - Posterior", "Semimembranosus - Posterior",
+                    "Semitendinosus - Posterior", "Popliteus - Posterior (Deep)", "Gastrocnemius - Posterior", "Rectus Femoris - Anterior",
                     "Vastus Lateralis - Anterior", "Vastus Medialis - Anterior","Vastus Intermedius - Anterior (Deep)", "Others"
             }, //Knee
 
-            {"Select Muscle*", "Gastrocnemius-Posterior", /*"superficial-Part of Triceps Surae",*/ "Soleus -Posterior", "Plantaris-Posterior",
-                    "Flexor Digitorum Longus(Deep)", "Flexor Hallucis Longus(Deep)", "Tibialis Posterior(Deep)", "Tibialis Anterior",
-                    "Extensor Digitorum Longus-Anterior", "Extensor Hallucis Longus-Anterior", "Peroneus Tertius-Anterior",
-                    "Peroneus Longus-Lateral", "Peroneus Brevis-Lateral", "Others"},    //Ankle
+            {"Select Muscle*", "Gastrocnemius - Posterior", /*"superficial-Part of Triceps Surae",*/ "Soleus - Posterior", "Plantaris - Posterior",
+                    "Flexor Digitorum Longus (Deep)", "Flexor Hallucis Longus (Deep)", "Tibialis Posterior (Deep)", "Tibialis Anterior",
+                    "Extensor Digitorum Longus - Anterior", "Extensor Hallucis Longus - Anterior", "Peroneus Tertius - Anterior",
+                    "Peroneus Longus - Lateral", "Peroneus Brevis - Lateral", "Others"},    //Ankle
 
-            {"Select Muscle*", "Rectus Femoris-Anterior", "Sartorius-Anterior", "Pectineus-Medial(Deep)",
-                    "Gluteus Medius And Gluteus Minimus-Gluteal", "Tensor Fasciae Latae-Gluteal ", "Adductor Longus -Medial",
-                    "Adductor Brevis -Medial","Adductor Magnus - Medial","Psoas Major","Iliacus","Gluteus Maximus -Gluteal","Biceps Femoris - Posterior",
-                    "Semimembranosus - Posterior", "Semitendinosus -Posterior", "Gracilis -Medial","Piriformis(Deep)",
-                    "Superior Gemellus(Deep)", "Obturator Internus(Deep)","Inferior Gemellus (Deep)", "Quadratus Femoris Obturator Externus (Deep)", "Others"},  //Hip //from 4 to 8 i have taken as different muscles as specified in the excell sheet
+            {"Select Muscle*", "Rectus Femoris - Anterior", "Sartorius - Anterior", "Pectineus - Medial (Deep)",
+                    "Gluteus Medius and Gluteus Minimus - Gluteal", "Tensor Fasciae Latae - Gluteal", "Adductor Longus - Medial",
+                    "Adductor Brevis - Medial","Adductor Magnus - Medial","Psoas Major","Iliacus","Gluteus Maximus - Gluteal","Biceps Femoris - Posterior",
+                    "Semimembranosus - Posterior", "Semitendinosus - Posterior", "Gracilis - Medial","Piriformis (Deep)",
+                    "Superior Gemellus (Deep)", "Obturator Internus (Deep)","Inferior Gemellus (Deep)", "Quadratus Femoris Obturator Externus (Deep)", "Others"},  //Hip //from 4 to 8 i have taken as different muscles as specified in the excell sheet
 
-            {"Select Muscle*", "Flexor Carpi Radialis", "Palmaris Longus", "Flexor Carpi Ulnaris", "Flexor Pollicis Longus(Deep)",
-                    "Flexor Digitorum Superficialis ( Intermediate )", "Flexor Digitorum  Profundus(Deep)", "Extensor Carpi Radialis Longus and Brevis",
+            {"Select Muscle*", "Flexor Carpi Radialis", "Palmaris Longus", "Flexor Carpi Ulnaris", "Flexor Pollicis Longus (Deep)",
+                    "Flexor Digitorum Superficialis (Intermediate)", "Flexor Digitorum  Profundus (Deep)", "Extensor Carpi Radialis Longus and Brevis",
                     "Extensor Digitorum", "Extensor Carpi Ulnaris", "Extensor Digiti Minimi", "Others"},  //Wrist
 
             {"Select Muscle*", "Pectoralis Major", "Latissimus Dorsi", "Teres Major", "Subscapularis", "Deltoid",  "Biceps",
                     "Coracobrachialis",  "Pectoralis Minor", "Serratus Anterior", "Infraspinatus", "Teres Minor", "Trapezius","Supraspinatus","Others"},   //Shoulder
 
             {"Select Muscle*","Biceps","Supinator (Deep)","Pronator Quadratus (Deep)", "Pronator Teres (Deep)", "Others"}, //forearm
-            {"Select Muscle*","Quadratus lumborum","Rectus abdominis","External oblique","Spinalis thoracis","Spinalis capitis",
-                    "Spinalis cervicis","Longissimus thoracis","Longissimus cervicis","Longissimus capitis",
-                    "Iliocostalis cervicis", "Iliocostalis thoracis","Iliocostalis lumborum","Semispinalis","Multifidus","Rotatores","Others"}, //Spine
+            {"Select Muscle*","Quadratus Lumborum","Rectus Abdominis","External Oblique","Spinalis Thoracis","Spinalis Capitis",
+                    "Spinalis Cervicis","Longissimus Thoracis","Longissimus Cervicis","Longissimus Capitis",
+                    "Iliocostalis Cervicis", "Iliocostalis Thoracis","Iliocostalis Lumborum","Semispinalis","Multifidus","Rotatores","Others"}, //Spine
             {"Select Muscle*","Others"}
     };
 
